89a01bffa48dd7f7fd301435e9893c216326964b
Çağrı Eroğlu <eroglu1994@gmail.com>
2017-04-29 14:30:09 +0300

    LoginScreen

---
+++ LoginScreen/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Windows.Controls.Primitives;
+namespace LoginScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var uc = new LoginUserControl();
+            pageTransition.ShowPage(uc);
+        }        
+        private void image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var uc = new LoginUserControl();
+            pageTransition.ShowPage(uc);
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            var uc = new RegisterUserControl();
+            pageTransition.ShowPage(uc);
+        }
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            var uc = new HelpUserControl();
+            pageTransition.ShowPage(uc);
+        }
+    }
+}
---
+++ LoginScreen/UserControls/HelpUserControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace LoginScreen
+{
+    /// <summary>
+    /// Interaction logic for HelpUserControl.xaml
+    /// </summary>
+    public partial class HelpUserControl : UserControl
+    {
+        public HelpUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            txtblockhelp.Visibility = Visibility.Hidden;
+            imghelp.Visibility = Visibility.Hidden;
+
+            pring.IsActive = true;
+            await Task.Delay(3000);
+            txtblockhelp.Visibility = Visibility.Visible;
+            imghelp.Visibility = Visibility.Visible;
+            pring.IsActive = false;
+
+
+        }
+    }
+}
